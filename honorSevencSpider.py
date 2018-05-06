7c43b406bb37cbc703b27638ea053529567535e0
QLWeilcf <1226824818@qq.com>
2018-05-06 18:28:58 +0800

    Add files via upload
    honor 7c Taobao Comment spider

--- honorSevencSpider.py
+++ honorSevencSpider.py
@@ -1,32 +1,75 @@
 #coding=UTF-8
 
 import requests
-from bs4 import BeautifulSoup
-from lxml import etree
+import json
+import re
 
-#获取单个页面
 def getOneUrl(url):
-    try:
-        r=requests.get(url,timeout=30)
-        r.raise_for_status()
-        r.encoding=r.apparent_encoding
-        return r.text
-    except:
-        return -1  #页面没正常下下来
+	try:
+		r = requests.get(url, timeout=30)
+		r.raise_for_status()
+		r.encoding = r.apparent_encoding
+		return r.text
+	except:
+		return -1  # 页面没正常下下来
 
-def parseUkUrl(url): #uoko的爬虫
-    text=getOneUrl(url)
-    if text!=-1:
-	    con=etree.HTML(text)
-	    rel_comment=con.xpath('//*[@id="J_Reviews"]/div/div[6]/table/tbody/tr/td[1]/div[1]/div[1]')
-	    for cn in rel_comment:
-		    print(cn.text)
+def getOverViewComment():#评论概览
+	oc_url1="https://rate.tmall.com/listTagClouds.htm?" \
+	       "itemId=565264660443&isAll=true&isInner=true&" \
+	       "t=1525589708011&_ksTS=1525589708015_920&callback=jsonp921"
+	oc_url = "https://rate.tmall.com/listTagClouds.htm?" \
+	         "itemId=565264660443&isAll=true&isInner=true" #这也是可以的
+	ovtxt=getOneUrl(oc_url)
+	print(ovtxt)
+	#jsonp921({"tags":{"dimenSum":8,…… loudList":""}})
+	if ovtxt==-1:
+		return
+	#ov2=re.compile('\{.+\}').search(ovtxt) #json的部分
+
+	ovc = json.loads('{'+ovtxt+'}')
+	print(ovc["tags"]["rateSum"])
+	for tc in ovc['tags']['tagClouds']:
+		print(tc['tag'],tc)
+
+
+def parseCommentJson(url, savep):  # 解析每个页面的json数据
+	text = getOneUrl(url)
+	if text == -1:
+		print('页面没正常获取', url)
+		return -1
+	hc = json.loads(text.strip().strip('()'))  # 除掉空格和首尾括号
+	if (hc['total'] == 0 or hc['comments'] == None):
+		return 0
+
+	print(hc['total'])
+
+	with open(savep, 'a+', encoding='utf-8') as wf:
+		for each_c in hc['comments']:
+			wstr = '{name},{date},{ct},{sku}'.format(name=each_c['user']['nick'],
+			                                         date=each_c['date'], ct=each_c['content'],
+			                                         sku=each_c['auction']['sku'])
+
+			aplst = each_c['appendList']
+			addstr = ''
+			if aplst != []:  # 有追评
+				for adict in aplst:
+					astr = ',{dac},{act}'.format(dac=str(adict['dayAfterConfirm']), act=adict['content'])
+					addstr = addstr+astr
+
+			wf.write(wstr+addstr+'\n')
+
+
 
 def honorComment():
-	curl='https://rate.tmall.com/list_detail_rate.htm?itemId=565264660443&spuId=937776110&sellerId=1114511827&order=3&currentPage=2&append=0&content=1&tagId=&posi=&picture=&ua=098%23E1hv1pvWvRhvUvCkvvvvvjiPPFMw1jtPRLzWljljPmPptj3nn2shtjDWPszy0jrUiQhvChCvCCptvpvhphvvvvGCvvpvvPMMvphvCyCCvvvvvbyCvm3vpvvvvvCvphCv2v9vvhUuphvZ7pvvp6nvpCBXvvC2p6CvHHyvvhn2kphvCyEmmvo4euyCvv3vpvoEOyCxu9yCvh1m9WGvItttp7QEfwAK5FXX8Z0vQC4AVAIaUExrV4t%2BmB%2BiaNpXVcEJEcqvaXTrjLhDNr1l5dUf836AAnpfHkx%2FaoF6AEu4X9Wfbj7Q3QhvChCCvvmrvpvEphUz7vZvpmqA9phv2Hiw5gCTzHi47%2BKZzsyCvvBvpvvv&isg=BMDAu6VfjlH6RHG2aXAzbq1ZkU6YS7QZpGrr6DpRiFtqtWDf4ll0o5anyR11BVzr&needFold=0&_ksTS=1523715183450_710&callback=jsonp711'
-	#c2='https://detail.tmall.com/item.htm?spm=a1z10.3-b-s.w4011-15291748836.63.418a6af4CjJ9NQ&id=565264660443&rn=09cbfdd9acb638b9e6aa4e9d568b2abe&abbucket=14&sku_properties=10004:653780895;5919063:6536025'
-	print(getOneUrl(curl)) #异步加载js，json的处理之后再补上。
-	parseUkUrl(curl)
+	for i in range(1,101):#结束页面可以自由改，如果要全量爬就设大一些,每个页面大概有20条 30115/20
+		url='https://rate.taobao.com/feedRateList.htm?auctionNumId={cid}&' \
+		    'currentPageNum={page}'.format(cid='565264660443',page=str(i))
+		savep='D:/FFOutput/honor7cComment_{page}.csv'.format(page=str(i)) #直接page=i 也是可以的
+		#page不迭代时，是写到同一个文件里，省得去合并了
+		if (parseCommentJson(url,savep)==0):
+			break
+
+getOverViewComment()
+#honorComment()
 
 
-honorComment()
